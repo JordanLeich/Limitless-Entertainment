62ef4cc9e2eabf89b0be1cc89b1faa568e5a6776
Jordan Leich <jordanleich@gmail.com>
2024-01-07 13:00:11 -0500

    major upgrades

--- js/templatemo-script.js
+++ js/templatemo-script.js
@@ -1,37 +1,33 @@
-var reqURL = "https://api.rss2json.com/v1/api.json?rss_url=" + encodeURIComponent("https://www.youtube.com/feeds/videos.xml?channel_id=");
+const reqURL = "https://api.rss2json.com/v1/api.json?rss_url=" +
+  encodeURIComponent("https://www.youtube.com/feeds/videos.xml?channel_id=");
 
 function loadVideo(iframe) {
-  $.getJSON(reqURL + iframe.getAttribute('cid'),
-    function (data) {
-      var videoNumber = (iframe.getAttribute('vnum') ? Number(iframe.getAttribute('vnum')) : 0);
-      console.log(videoNumber);
-      var link = data.items[videoNumber].link;
-      id = link.substr(link.indexOf("=") + 1);
-      iframe.setAttribute("src", "https://youtube.com/embed/" + id + "?controls=0&autoplay=1");
-    }
-  );
+  $.getJSON(reqURL + iframe.getAttribute('cid'), function (data) {
+    const videoNumber = (iframe.getAttribute('vnum') ? Number(iframe.getAttribute('vnum')) : 0);
+    console.log(videoNumber);
+    const link = data.items[videoNumber].link;
+    const id = link.substr(link.indexOf("=") + 1);
+    iframe.setAttribute("src", `https://youtube.com/embed/${id}?controls=0&autoplay=1`);
+  });
 }
 
-var iframes = document.getElementsByClassName('latestVideoEmbed');
-for (var i = 0, len = iframes.length; i < len; i++) {
-  loadVideo(iframes[i]);
-}
-
+const iframes = document.getElementsByClassName('latestVideoEmbed');
+Array.from(iframes).forEach(loadVideo);
 
 const initBg = (autoplay = true) => {
-  const bgImgsNames = ['diagoona-bg-1.jpg', 'diagoona-bg-2.jpg', 'diagoona-bg-3.jpg'];
-  const bgImgs = bgImgsNames.map(img => "img/" + img);
+  const bgImgs = ['forza.jpg', 'ascension.jpg', 'skyrim.jpg', 'anor.jpg', 'gorod.png']
+    .map(img => `img/${img}`);
 
-  $.backstretch(bgImgs, { duration: 4000, fade: 500 });
+  $.backstretch(bgImgs, { duration: 3000, fade: 3000 });
 
   if (!autoplay) {
     $.backstretch('pause');
   }
-}
+};
 
 const setBg = id => {
   $.backstretch('show', id);
-}
+};
 
 const setBgOverlay = () => {
   const windowWidth = window.innerWidth;
@@ -41,18 +37,25 @@ const setBgOverlay = () => {
   $('.tm-bg').height(bgHeight);
 
   if (windowWidth > 768) {
-    tmBgLeft.css('border-left', `0`)
-      .css('border-top', `${bgHeight}px solid transparent`);
+    tmBgLeft.css('border-left', '0').css('border-top', `${bgHeight}px solid transparent`);
   } else {
-    tmBgLeft.css('border-left', `${windowWidth}px solid transparent`)
-      .css('border-top', `0`);
+    tmBgLeft.css('border-left', `${windowWidth}px solid transparent`).css('border-top', '0');
   }
-}
+};
 
 $(document).ready(function () {
-  const autoplayBg = true;	// set Auto Play for Background Images
+  const autoplayBg = true; // set Auto Play for Background Images
   initBg(autoplayBg);
   setBgOverlay();
+
+  const bgControlsWrapper = $(".tm-bg-controls-wrapper");
+  const bgControlTemplate = '<li class="tm-bg-control" data-id="%id%"></li>';
+
+  // Dynamically generate background controls based on the number of images
+  const bgImgsCount = 0; // Adjust this based on the actual number of background images
+  for (let i = 0; i < bgImgsCount; i++) {
+    bgControlsWrapper.append(bgControlTemplate.replace('%id%', i));
+  }
 
   const bgControl = $('.tm-bg-control');
   bgControl.click(function () {
@@ -65,7 +68,7 @@ $(document).ready(function () {
   $(window).on("backstretch.after", function (e, instance, index) {
     const bgControl = $('.tm-bg-control');
     bgControl.removeClass('active');
-    const current = $(".tm-bg-controls-wrapper").find(`[data-id=${index}]`);
+    const current = bgControlsWrapper.find(`[data-id=${index}]`);
     current.addClass('active');
   });
 
@@ -73,3 +76,4 @@ $(document).ready(function () {
     setBgOverlay();
   });
 });
+
